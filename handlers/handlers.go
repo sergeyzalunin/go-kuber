55295f26ed0ace1e59991b4634091a773c174b2e
Sergey Zalunin <z.a.l.u.n.i.n.s.e.r.g@gmail.com>
2021-02-21 19:04:51 +0300

    added gracefull shutdown and kubernates handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,12 +1,31 @@
 package handlers
 
 import (
+	"log"
+	"sync/atomic"
+	"time"
+
 	"github.com/gorilla/mux"
 )
 
+const isReadyDuration = 10 * time.Second
+
 // Router register necessary routes and returns an instance of a router.
 func Router(buildTime, commit, release string) *mux.Router {
+	isReady := &atomic.Value{}
+	isReady.Store(false)
+
+	go func() {
+		log.Printf("Readyz probe is negative by default...")
+		time.Sleep(isReadyDuration)
+		isReady.Store(true)
+		log.Printf("Readyz probe is positive.")
+	}()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/home", home(buildTime, commit, release)).Methods("GET")
+	r.HandleFunc("/healthz", healthz)
+	r.HandleFunc("/readyz", readyz(isReady))
+
 	return r
 }
--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/sergeyzalunin/go-kuber/handlers"
 	"github.com/sergeyzalunin/go-kuber/version"
@@ -22,6 +25,28 @@ func main() {
 
 	r := handlers.Router(version.BuildTime, version.Commit, version.Release)
 
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, syscall.SIGTERM)
+
+	srv := http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
 	log.Print("The service is ready to listen and serve.")
-	log.Fatal(http.ListenAndServe(":"+port, r))
+
+	switch <-interrupt {
+	case os.Interrupt:
+		log.Print("Got SIGINT...")
+	case syscall.SIGTERM:
+		log.Print("Got SIGTERM...")
+	}
+
+	log.Print("The service is shutting down")
+	srv.Shutdown(context.Background())
+	log.Print("Done")
 }
